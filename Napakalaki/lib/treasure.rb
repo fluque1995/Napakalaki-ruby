d8b1b03cfd33359d74310cd6f4286eebec3d487e
antonio <antonio@antonio-Aspire-5742>
2015-03-26 13:08:16 +0100

    creando treasure

---
+++ Napakalaki/lib/treasure.rb
@@ -0,0 +1,18 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+class Treasure
+  def initialize(n, g, min, max, t)
+    @name = n
+    @goldCoins = g
+    @minBonus = min
+    @maxBonus = max
+    @type = t
+  end
+  
+  attr_reader :name
+  attr_reader :goldCoins, :minBonus, :maxBonus
+  attr_reader :type
+  
+end
