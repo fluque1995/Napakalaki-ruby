8540c333d2d862788bf2d906abef80e25dc60a8e
antonio <antonio@antonio-Aspire-5742>
2015-03-26 13:07:25 +0100

    Creando dice

---
+++ Napakalaki/lib/dice.rb
@@ -0,0 +1,16 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+class Dice
+  include Singleton
+  
+  def initialize
+  
+  end
+  
+  def nextNumber()
+    
+  end
+  
+end
